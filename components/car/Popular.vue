e0ba7e04cac8ba128127dbb2c49722054108f971
Imane-Acherrat <imaneacherrat1998@gmail.com>
2023-12-29 09:25:13 +0100

    Fetch popular cars

--- components/car/Popular.vue
+++ components/car/Popular.vue
@@ -9,53 +9,5 @@
   </div>
 </template>
 <script setup lang="ts">
-// const runtimeConfig = useRuntimeConfig();
-const data = [
-  {
-    name: "Honda Accord",
-    type: "Sedan",
-    gasolineLiter: 65,
-    kindOfTransition: "Manual",
-    people: 5,
-    pricePerDay: 60,
-    id: "honda-accord",
-    img: "https://dm-assignment-commonshare.koyeb.app/img/1.png",
-  },
-  {
-    name: "Mazda 3",
-    type: "Hatchback",
-    gasolineLiter: 55,
-    kindOfTransition: "Automatic",
-    people: 5,
-    pricePerDay: 45,
-    id: "mazda-3",
-    img: "https://dm-assignment-commonshare.koyeb.app/img/2.png",
-  },
-  {
-    name: "Volkswagen Golf",
-    type: "Hatchback",
-    gasolineLiter: 50,
-    kindOfTransition: "Manual",
-    people: 5,
-    pricePerDay: 55,
-    id: "volkswagen-golf",
-    img: "https://dm-assignment-commonshare.koyeb.app/img/3.png",
-  },
-  {
-    name: "Audi A4",
-    type: "Sedan",
-    gasolineLiter: 70,
-    kindOfTransition: "Automatic",
-    people: 5,
-    pricePerDay: 75,
-    id: "audi-a4",
-    img: "https://dm-assignment-commonshare.koyeb.app/img/4.png",
-  },
-];
-// const p = await useFetch(
-//   "https://dm-assignment-commonshare.koyeb.app/api/cars"
-// );
-
-// console.log(p);
-// console.log(data);
+const { data } = await useFetch("/api/cars/popular");
 </script>
